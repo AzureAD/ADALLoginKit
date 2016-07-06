830efaff679cbb46345f9852b8fba4afe79c53e7
Brandon Werner <brandwe@microsoft.com>
2016-07-06 14:18:48 -0700

    Initial Commit

---
+++ ADALLoginKit/ADALLoginKit.h
@@ -0,0 +1,19 @@
+//
+//  ADALLoginKit.h
+//  ADALLoginKit
+//
+//  Created by Brandon Werner on 7/6/16.
+//  Copyright © 2016 Microsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ADALLoginKit.
+FOUNDATION_EXPORT double ADALLoginKitVersionNumber;
+
+//! Project version string for ADALLoginKit.
+FOUNDATION_EXPORT const unsigned char ADALLoginKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ADALLoginKit/PublicHeader.h>
+
+
